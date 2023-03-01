0ba999a8760f5983545596030d3b18f44ef87d44
Quy Phan <quy.phan903@gmail.com>
2023-02-28 21:44:40 -0800

    3rd change

---
+++ 8kyu/Return-negative.js
@@ -0,0 +1,8 @@
+//In this simple assignment you are given a number and have to make it negative. 
+//But maybe the number is already negative?
+
+
+// Myt solution
+function makeNegative(num) {
+    return -Math.abs(num);
+  }
